f72260d60c408247298a9c02c761934a9f2be2ca
Bane.Shi <fengxiaotx@163.com>
2016-04-24 17:08:13 +0800

    [Bane.Shi]

---
+++ src/form/field/date-range-single-field.vue
@@ -0,0 +1,18 @@
+<!--
+Created by Bane.Shi.
+Copyright MoenSun
+User: Bane.Shi
+Date: 16/4/24
+Time: 17:08-->
+<template>
+
+</template>
+<script>
+    export default{
+        components:{
+        }
+    }
+</script>
+<style>
+
+</style>
